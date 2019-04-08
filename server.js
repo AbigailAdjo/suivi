0043707d627102645114b68988bf38affa5da58c
papangom.ksk <papangom.ksk@gmail.com>
2019-04-08 15:35:02 +0000

    8 commit

--- server.js
+++ server.js
@@ -3,15 +3,15 @@
  * on 3/18/19.
  */
 
-const express = require('express'),
+let express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
 port = process.env.PORT || 3300; //3000
 
 
-const mysql = require('mysql');
+let mysql = require('mysql');
 // connection configurations
-const mc = mysql.createConnection({
+let mc = mysql.createConnection({
     host     : 'nesz.myd.sharedbox.com',
     user     : 'nesz_postecash',
     port     : '3306',
@@ -19,9 +19,26 @@ const mc = mysql.createConnection({
     database : 'nesz_postecash'
     //socketPath: '/tmp/mysql.sock'
 });
-
-// connect to database
 mc.connect();
+handleDisconnect(mc);
+function handleDisconnect(client) {
+    client.on('error', function (error) {
+        if (!error.fatal) return;
+        if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+        console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+        // NOTE: This assignment is to a variable from an outer scope; this is extremely important
+        // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+        // to `global.mysqlClient =` in node.
+        mysqlClient = mysql.createConnection(client.config);
+        handleDisconnect(mysqlClient);
+        mysqlClient.connect();
+    });
+};
+
+
+
 
 app.listen(port);
 
