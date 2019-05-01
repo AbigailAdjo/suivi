f4b14aebb50201c7ddf589343aeb3a9cc45ab0a3
papangom.ksk <papangom.ksk@gmail.com>
2019-05-01 21:56:31 +0000

    Push p

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ function handleDisconnect(client) {
         // NOTE: This assignment is to a variable from an outer scope; this is extremely important
         // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
         // to `global.mysqlClient =` in node.
-        mysqlClient = mysql.createConnection(client.config);
+        const mysqlClient = mysql.createConnection(client.config);
         handleDisconnect(mysqlClient);
         mysqlClient.connect();
     });
